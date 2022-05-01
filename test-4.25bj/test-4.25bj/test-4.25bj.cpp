d1a220d998877d3ec06b78dc31b609c1e7ac5ece
Wang-Running <1767479196@qq.com>
2022-05-01 23:46:04 +0800

    命名空间，缺省参数

--- test-4.25bj/test-4.25bj/test-4.25bj.cpp
+++ test-4.25bj/test-4.25bj/test-4.25bj.cpp
@@ -1,8 +1,169 @@
 #define _CRT_SECURE_NO_WARNINGS
+
+//#include <iostream>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "List.h"
+//#include "Queue.h"
+//using namespace A::data;
+//int main()
+//{
+//	struct Node n1;
+//	a = 3;
+//	b = 4;
+//	struct cache ::Node n3;
+//	printf("%d %d",a,b);
+//	return 0;
+//}
+
+
+
+//命名冲突
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+////定义全局变量
+//namespace A
+//{
+//	int rand = 0;
+//}
+//
+//int main()
+//{
+//	printf("%d ",rand);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+////定义全局变量
+//int rand = 0;
+//
+//namespace A
+//{
+//	int rand = 2;
+//}
+//
+//int main()
+//{
+//	int rand = 1;
+//	printf("%d ",A::rand);
+//	return 0;
+//}
+
+
+
+
+
+//namespace anm  //命名空间域
+//{
+//	int rand = 0;
+//}
+//
+//int a = 0;
+
+//int main()
+//{
+//	int a = 1;
+//
+//	//::域作用限定符
+//
+//	printf("%d\n",anm::rand);
+//	printf("%d\n", a);
+//	printf("%d\n", ::a);
+//	return 0;
+//}
+
+//using namespace A;
+//int main()
+//{
+//	struct data::Node n1;
+//	struct data::Node n2;
+//	struct data::Node n3;
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//namespace C
+//{
+//	int f = 0;
+//	int rand = 1;
+//	namespace D
+//	{
+//		int d = 4;
+//		namespace E
+//		{
+//			int e = 5;
+//			char g = 'p';
+//		}
+//	}
+//}
+//
+////释放E中所有内容
+//using namespace C::D::E;
+//int main()
+//{
+//	e += 1;
+//	printf("%d\n",e);
+//	printf("%d\n", rand);
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "hello word" << endl;
+//	int a = 0;
+//	cin >> a;
+//	cout << a<<endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;//std 封C++库的命名空间
+
+//全放不安全，不放太麻烦
+//放一些常用的
+//using std::cout;
+
+
+//int cout = 0;
+
+//int main()
+//{
+//	/*cout << "hello word" << endl;*/
+//	cout << "hello word" << std::endl;
+//	cout << "hello word" << std::endl;
+//
+//	//std::cout << cout << std::endl;
+//
+//	return 0;
+//}
+
 #include <iostream>
 using namespace std;
+
+//全缺省参数
+//void TestA(int a = 5, int b = 10, int c = 15)
+//{
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//	cout << "c = " << c << endl<<endl;
+//}
+
+//半缺省参数（缺省部分参数）
+void TestA(int a , int b = 10, int c = 15)
+{
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl << endl;
+}
+
 int main()
 {
-
+	TestA(1);
+	TestA(1,5);
+	TestA(1,5,6);
 	return 0;
 }
