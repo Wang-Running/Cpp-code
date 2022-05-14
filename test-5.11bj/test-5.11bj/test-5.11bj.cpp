719505b337daf1c29e065c8334f6a3e7dc959d50
Wang-Running <1767479196@qq.com>
2022-05-14 23:54:47 +0800

    类和对象一

--- test-5.11bj/test-5.11bj/test-5.11bj.cpp
+++ test-5.11bj/test-5.11bj/test-5.11bj.cpp
@@ -17,7 +17,7 @@
 //{
 //	return 0;
 //}
-
+#include <cassert>
 #include <iostream>
 using namespace std;
 
@@ -40,11 +40,71 @@ private:
 	int _num;
 };
 
+//int main()
+//{
+//	Food egg;
+//	egg.Init("鸡蛋",2);
+//	egg.Print();
+//	
+//	return 0;
+//}
+
+//封装
+//struct Stack
+//{
+//	int* a;
+//	int top;
+//	int capacity;
+//};
+//
+//void StackInit(struct Stack* ps)
+//{
+//	ps->a = NULL;
+//	ps->top = 0;
+//	ps->capacity = 0;
+//}
+//
+//int StackTop(struct Stack* ps)
+//{
+//	assert(ps);
+//	assert(ps->top>0);
+//	return ps->a[ps->top - 1];
+//}
+
+class Stack
+{
+public:
+	void StackInit(struct Stack* ps)
+	{
+		ps->_a = NULL;
+		ps->_top = 0;
+		ps->_capacity = 0;
+	}
+
+	int StackTop(struct Stack* ps)
+	{
+		assert(ps);
+		assert(ps->_top>0);
+		return ps->_a[ps->_top - 1];
+	}
+private:
+	int* _a;
+	int _top;
+	int _capacity;
+};
+
+
+
 int main()
 {
-	Food egg;
-	egg.Init("鸡蛋",2);
-	egg.Print();
-	
+	Stack ST;
+	ST.StackInit(&ST);
+
+	//规范操作
+	int top = ST.StackTop(&ST);
+
+	//不规范操作
+	int top1 = ST._a[ST._top]; //越界
+	int top2 = ST._a[ST._top-1];//正常
 	return 0;
 }
