c860cab2bcf4278e669eaac2a23bda07bf50be12
Wang-Running <1767479196@qq.com>
2022-05-18 23:28:46 +0800

    构造 析构 拷贝构造

--- test-5.15bj/test-5.15bj/test-5.15bj.cpp
+++ test-5.15bj/test-5.15bj/test-5.15bj.cpp
@@ -1,18 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 //日期类示例空类下生成的六个默认函数
 
+class A
+{
+public:
+	A()
+	{
+		a = 5;
+	}
+	~A()
+	{
+		a = 0;
+	}
+private:
+	int a;
+};
+
 class Stack
 {
 public:
-
+	Stack(int capacity=10)
+	{
+		_a = (int*)malloc(sizeof(int)*capacity);
+		assert(_a);
+		_top = 0;
+		_capacity = capacity;
+	}
+	~Stack()
+	{
+		free(_a);
+		_a = nullptr;
+		_top = _capacity = 0;
+	}
 private:
 	int* _a;
 	int _top;
 	int _capacity;
 };
+
 
 class Date
 {
@@ -27,12 +56,12 @@ public:
 	//1.默认生成构造函数
 
 	//2.无参数构造函数
-	Date()
-	{
-		_year = 2022;
-		_month = 5;
-		_day = 15;
-	}
+	//Date()
+	//{
+	//	_year = 2022;
+	//	_month = 5;
+	//	_day = 15;
+	//}
 	//3.带参的构造函数
 	Date(int year=2000,int month=1,int day=1)
 	{
@@ -40,6 +69,20 @@ public:
 		_month = month;
 		_day = day;
 	}
+
+	//Date(Date& d)
+	//{
+	//	_year = d._year;
+	//	_month = d._month;
+	//	_day = d._day;
+	//}
+	~Date()
+	{
+		_year = 1;
+		_month = 1;
+		_day = 1;
+	}
+
 	void Print()
 	{
 		cout << _year << "-" << _month << "-" << _day << endl;
@@ -48,11 +91,26 @@ private:
 	int _year;
 	int _month;
 	int _day;
+	A aa;
 };
 
 int main()
 {
-	Date d1(2018,1);
-	Date d2;
+	Date d1;
+	Date d2(d1);
+	d1.Print();
+	d2.Print();
+	//Stack ST1(1);
+	//Stack ST2(2);
 	return 0;
 }
+
+
+//class MyQueue
+//{
+//public:
+//	//
+//private:
+//	Stack ST1(1);
+//	Stack ST2(2);
+//};
