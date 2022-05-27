c22d38422594cf389fd8f8f2cee3bc95b5d1ead6
Wang-Running <1767479196@qq.com>
2022-05-27 17:02:43 +0800

    类和对象三

--- test-5.23bj/test-5.23bj/test-5.23bj.cpp
+++ test-5.23bj/test-5.23bj/test-5.23bj.cpp
@@ -85,48 +85,134 @@ using namespace std;
 //	return 0;
 //}
 
+//static 成员
 
-class Day
+//class Day
+//{
+//public:
+//	Day(int year)
+//		:_bb(year)
+//		, _b(_bb)
+//	{
+//		_count++;
+//		cout << "构造" << endl;
+//	}
+//	Day(const Day& d)
+//	{
+//		_count++;
+//		cout << "拷贝构造" << endl;
+//	}
+//	void Print() const
+//	{
+//		cout << _b << " " << _bb << endl;
+//	}
+//	//static int _count;  //声明
+//	static int Getcount()
+//	{
+//		return _count;
+//	}
+//private:
+//	int _b;
+//	int _bb;
+//	static int _count;  //声明
+//};
+//
+////定义
+//int Day::_count = 1;
+//
+//int main()
+//{
+//	Day d1(2001);
+//	const Day& d2 = 2002;
+//	d2.Print();
+//	cout << d2.Getcount() << endl;
+//	cout << d1.Getcount() << endl;
+//	cout << Day::Getcount() << endl;
+//
+//	return 0;
+//}
+
+//友元类
+
+//class A
+//{
+//public:
+//	A(int i,int j)
+//		:_a(i)
+//		, _aa(j)
+//	{}
+//	void GetB()
+//	{
+//		BigB._b = _a;
+//		BigB._bb = _aa;
+//	}
+//private:
+//	int _a;
+//	int _aa;
+//	B BigB;
+//};
+//
+//class B
+//{
+//	friend class A;
+//public:
+//	B(int i=22, int j=222)
+//		:_b(i)
+//		, _bb(j)
+//	{}
+//private:
+//	int _b;
+//	int _bb;
+//};
+//int main()
+//{
+//	A AX(1,12);
+//	B BX;
+//	AX.GetB();
+//}
+
+//内部类
+class A
 {
 public:
-	Day(int year)
-		:_bb(year)
-		, _b(_bb)
+	A(int i,int j)
+		:_a(i)
+		, _aa(j)
+	{}
+	//内部类B
+	//默认B是A的友元类
+	class B
 	{
-		_count++;
-		cout << "构造" << endl;
-	}
-	Day(const Day& d)
+	public:
+		B(int i = 22, int j = 222)
+			:_b(i)
+			, _bb(j)
+		{}
+		//可以通过A类对象访问其私有成员
+		void GetA()
+		{
+			A AX(12,13);
+			cout << AX._a << endl;
+		}
+	private:
+		int _b;
+		int _bb;
+	};
+	//外部类A不能通过内部类B的对象访问其私有成员
+	void GetB()
 	{
-		_count++;
-		cout << "拷贝构造" << endl;
-	}
-	void Print() const
-	{
-		cout << _b << " " << _bb << endl;
-	}
-	//static int _count;  //声明
-	static int Getcount()
-	{
-		return _count;
+		B BX;
+		//cout << BX._b << endl;
 	}
 private:
-	int _b;
-	int _bb;
-	static int _count;  //声明
+	int _a;
+	int _aa;
 };
 
-//定义
-int Day::_count = 1;
 
 int main()
 {
-	Day d1(2001);
-	const Day& d2 = 2002;
-	d2.Print();
-	cout << d2.Getcount() << endl;
-	cout << d1.Getcount() << endl;
-	cout << Day::Getcount() << endl;
-
-	return 0;
+	A AX(1,12);
+	A::B BX;
+	AX.GetB();
 }
