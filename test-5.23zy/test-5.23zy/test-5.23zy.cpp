604763b0826f65b25d16363f0f00f9324841ca66
Wang-Running <1767479196@qq.com>
2022-06-10 21:49:08 +0800

    日期类题目

--- test-5.23zy/test-5.23zy/test-5.23zy.cpp
+++ test-5.23zy/test-5.23zy/test-5.23zy.cpp
@@ -1,13 +1,70 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+////先把一年内每月间的差值求出
+//int days[13] = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+//
+////判断闰年
+//bool leapyear(int year)
+//{
+//	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+//	{
+//		return true;
+//	}
+//	else
+//		return false;
+//}
+//
+////确定年有多少天
+//int yeardays(int year)
+//{
+//	if (leapyear(year))
+//	{
+//		return 366;
+//	}
+//	else
+//		return 365;
+//}
+//
+//int sum(int year, int month, int day)
+//{
+//	int sum = 0;
+//	//从元年开始算
+//	for (int i = 0; i<year; i++)
+//	{
+//		sum += yeardays(i);
+//	}
+//	if (leapyear(year))
+//	{
+//		sum += days[month - 1] + day+1;
+//	}
+//	else
+//	{
+//		sum += days[month - 1] + day;
+//	}
+//	return sum;
+//}
+//
+//int main()
+//{
+//	int year1, month1, day1;
+//	int year2, month2, day2;
+//	while (~scanf("%04d%02d%02d", &year1, &month1, &day1))
+//	{
+//		scanf("%04d%02d%02d", &year2, &month2, &day2);
+//		printf("%d\n", abs(sum(year1, month1, day1) -
+//			sum(year2, month2, day2)) + 1);
+//	}
+//	return 0;
+//}
+
+//KY222 打印日期 -- c版
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-//先把一年内每月间的差值求出
-int days[13] = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
-
-//判断闰年
 bool leapyear(int year)
 {
 	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
@@ -18,45 +75,60 @@ bool leapyear(int year)
 		return false;
 }
 
-//确定年有多少天
-int yeardays(int year)
+void time(int year, int day, int days1[], int days2[])
 {
 	if (leapyear(year))
 	{
-		return 366;
+		int i = 1;
+		while (i<13)
+		{
+			int d;
+			if (day > days2[i] && day <= days2[i + 1])
+			{
+				d = (day - days2[i]);
+				printf("%04d-%02d-%02d\n", year, i + 1, d);
+				break;
+			}
+			if (day <= days2[i])
+			{
+				d = day - days2[i - 1];
+				printf("%04d-%02d-%02d\n", year, i, d);
+				break;
+			}
+			i++;
+		}
 	}
 	else
-		return 365;
-}
-
-int sum(int year, int month, int day)
-{
-	int sum = 0;
-	//从元年开始算
-	for (int i = 0; i<year; i++)
 	{
-		sum += yeardays(i);
+		int i = 1;
+		while (i<13)
+		{
+			int d;
+			if (day > days1[i] && day <= days1[i + 1])
+			{
+				d = (day - days1[i]);
+				printf("%04d-%02d-%02d\n", year, i + 1, d);
+				break;
+			}
+			if (day == days1[i])
+			{
+				d = day - days1[i - 1];
+				printf("%04d-%02d-%02d\n", year, i, d);
+				break;
+			}
+			i++;
+		}
 	}
-	if (leapyear(year))
-	{
-		sum += days[month - 1] + day+1;
-	}
-	else
-	{
-		sum += days[month - 1] + day;
-	}
-	return sum;
 }
 
 int main()
 {
-	int year1, month1, day1;
-	int year2, month2, day2;
-	while (~scanf("%04d%02d%02d", &year1, &month1, &day1))
+	int days1[13] = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+	int days2[13] = { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 };
+	int year = 2000, day = 55;
+	while (~scanf("%d %d", &year, &day))
 	{
-		scanf("%04d%02d%02d", &year2, &month2, &day2);
-		printf("%d\n", abs(sum(year1, month1, day1) -
-			sum(year2, month2, day2)) + 1);
+		time(year, day, days1, days2);;
 	}
 	return 0;
 }
