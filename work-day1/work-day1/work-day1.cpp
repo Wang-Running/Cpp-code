942b1000dbf350a2847c8aa3876c11f0dac968f4
Wang-Running <1767479196@qq.com>
2022-07-13 09:39:25 +0800

    Sum-work-1

--- work-day1/work-day1/work-day1.cpp
+++ work-day1/work-day1/work-day1.cpp
@@ -72,55 +72,82 @@
 //	printf("%d\n", count);
 //}
 
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//
+////思路：将全部转化为字符
+////计算两个字符串大小及位数
+////提取末尾字串比较
+//
+//string Num_string(int val)
+//{
+//	//1.先计算位数
+//	//2.转换字符然后尾插，最后逆置
+//	string s;
+//	int bit = 0;
+//	while (val)
+//	{
+//		bit++;
+//		s += (val % 10 + '0');
+//		val = (val - (val % 10)) / 10;
+//	}
+//	reverse(s.begin(), s.end());
+//	return s;
+//}
+//
+//int main(){
+//	//计数
+//	int count = 0;
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i <= n; i++)
+//	{
+//		int num = i*i;
+//		if (i == num)
+//		{
+//			count++;
+//		}
+//		else
+//		{
+//			string s1 = Num_string(i);
+//			string s2 = Num_string(num);
+//			size_t size1 = s1.size();
+//			size_t size2 = s2.size();
+//			string s3 = s2.substr(size2 - size1, size1);
+//			if (s1 == s3)
+//			{
+//				count++;
+//			}
+//		}
+//	}
+//	cout << count << endl;
+//}
+
+//OR86 返回小于 N 的质数个数
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
-
-//思路：将全部转化为字符
-//计算两个字符串大小及位数
-//提取末尾字串比较
-
-string Num_string(int val)
+int main()
 {
-	//1.先计算位数
-	//2.转换字符然后尾插，最后逆置
-	string s;
-	int bit = 0;
-	while (val)
-	{
-		bit++;
-		s += (val % 10 + '0');
-		val = (val - (val % 10)) / 10;
-	}
-	reverse(s.begin(), s.end());
-	return s;
-}
-
-int main(){
-	//计数
-	int count = 0;
 	int n;
 	cin >> n;
-	for (int i = 0; i <= n; i++)
+	//计数
+	int count = 0;
+	for (int i = 2; i <= n; i++)
 	{
-		int num = i*i;
-		if (i == num)
+		for (int j = 2; j <= i; j++)
 		{
-			count++;
-		}
-		else
-		{
-			string s1 = Num_string(i);
-			string s2 = Num_string(num);
-			size_t size1 = s1.size();
-			size_t size2 = s2.size();
-			string s3 = s2.substr(size2 - size1, size1);
-			if (s1 == s3)
+			if (j<i && i % j == 0)
+			{
+				break;
+			}
+			if (i == j)
 			{
 				count++;
 			}
 		}
 	}
 	cout << count << endl;
+	return 0;
 }
